0e5ea29a5684f8a8badd7b11592830dbdf0a1c72
Genesis Ian Palaganas <palaganas.genesis@gmail.com>
2017-09-26 00:34:35 +0800

    Change Test2.java message

--- Test2.java
+++ Test2.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Main{
     public static void main(String arg[]){
-        String i = "We have arrived!";
-	System.out.println(i);
+        String i = "Banana has arrived!";
+        System.out.println(i);
     }
 }
